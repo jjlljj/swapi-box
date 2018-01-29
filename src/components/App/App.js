173e6942c5f231d4259f4defebcc3fa3b04782c5
James Logue <logueja@gmail.com>
2018-01-29 15:57:12 -0700

    Add App directory, update file tree

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+   
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        i am an apppppppp
+      </div>
+    );
+  }
+}
+
+export default App;
