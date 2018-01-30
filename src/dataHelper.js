28ddb7a96f1661239e218b42f568839fde136f55
James Logue <logueja@gmail.com>
2018-01-30 14:44:12 -0700

    Pseudo code api fetch and data clean

---
+++ src/dataHelper.js
@@ -0,0 +1,32 @@
+// MOVIES
+// fetch ( https://swapi.co/api/films/ )
+// --> array of Movies --> pick one
+// --> opening crawl: movie. opening_crawl
+
+
+// PEOPLE 
+// Fetch ( https://swapi.co/api/people/ )
+//  --> name: result.name
+//  --> species: fetch (result.species) => speciesResult.name
+//  --> homeworld : fetch ( result.homeworld ) => worldResult
+//  --> homeworld: worldResult.name
+//  --> homeworld population: worldResult.population
+// FAV BUTTON
+
+
+// PLANETS
+// Fetch ( https://swapi.co/api/planets/ )
+// --> name: result.name
+// --> terrain: result.terrain
+// --> population: result.climate
+// --> climate: result.climate
+// --> residents: []... fetch each resident ==> residentResult.name
+// FAV BUTTON
+
+
+// VEHICLES
+// Fetch ( https://swapi.co/api/vehicles/ )
+// --> name: results.name
+// --> model: results.model
+// --> class: result.vehicle_class
+// --> number of passengers: result.passengers
