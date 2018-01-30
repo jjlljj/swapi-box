d70a2b8d0435d84a6982a6c9988b253864e1b3e3
James Logue <logueja@gmail.com>
2018-01-29 18:31:44 -0700

    Add imports and initial structure to Card component and test

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import './Card.css';
+
+const Card = () => {
+  return (
+    <div> i am card </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { shallow } from 'enzyme';
+import Card from './Card';
+
+describe('Card', () => {
+
+  it('should exist', () => {
+  
+  })
+
+})
