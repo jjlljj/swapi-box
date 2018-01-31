607ce627d96a2c7d38420951c7ad7422f77a7690
James Logue <logueja@gmail.com>
2018-01-30 19:46:40 -0700

    Add localstorage and set api call data for functionality buildout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,19 +9,36 @@ class App extends Component {
     super();
     this.state = {
       openingScroll: {},
-      people: [],
-      planets: [],
-      vehicles: []
+      people: null,
+      planets: null,
+      vehicles: null
     };
   }
 
   async componentDidMount() {
-    //const openingScroll = await getScroll()
-    //const people = await getPeople()
-    const planets = await getPlanets()
-    const vehicles = await getVehicles()
-    this.setState({ planets, vehicles})
+    const openingScroll = await getScroll()
+    let { people, planets, vehicles } = this.lastFromSto()
 
+    people = people || await getPeople()
+    planets = planets || await getPlanets()
+    vehicles = vehicles || await getVehicles()
+    
+    this.setState({ openingScroll, people, planets, vehicles }, ()=> {
+      console.log(this.state)
+    } )
+
+  }
+
+  dataToSto({ people, planets, vehicles }) {
+    const toSet = JSON.stringify({ people, planets, vehicles })
+    localStorage.setItem('swapiData', toSet)
+  }
+
+  lastFromSto() {
+    const swapiData = localStorage.getItem('swapiData')
+    const { people, planets, vehicles } = JSON.parse(swapiData) || this.state 
+
+    return { people, planets, vehicles }
   }
 
   render() {
