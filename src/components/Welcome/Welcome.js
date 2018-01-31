165a1f8f2e72b74a9985ba050a1e1aa4ea49279e
James Logue <logueja@gmail.com>
2018-01-30 20:52:15 -0700

    Render fetched movie scrawl, style

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -5,11 +5,15 @@ import './Welcome.css';
 const Welcome = ({ episode, title, text}) => {
 
   return (
-    <div className="welcome-crawl"> 
-      <h4>{episode}</h4>
-      <h2>{title}</h2>
-      <p>{text}</p>
-    </div>
+    <section className="welcome-crawl"> 
+      <div className="crawl-wrap">
+        <div className="title-wrap">
+          <h4>{episode}</h4>
+          <h2>{title}</h2>
+        </div>
+        <p>{text}</p>
+      </div>
+    </section>
   );
 };
 
