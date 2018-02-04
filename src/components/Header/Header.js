16eb23cf88c2496eda6623c98fbe72cb6851fa71
James Logue <logueja@gmail.com>
2018-02-04 15:45:47 -0700

    Clean up lint errors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Header.css';
 import Nav from '../Nav/Nav';
-import { Link, NavLink } from 'react-router-dom';
-const starWarsLogo = require('./assets/star-wars-logo-red.png')
+import { Link } from 'react-router-dom';
+const starWarsLogo = require('./assets/star-wars-logo-red.png');
 
 const Header = () => {
   return (
     <header className="site-header">
-      <Link to='/'><img src={starWarsLogo} className="banner-text" alt={'Star wars'}/></Link>
+      <Link to='/'>
+        <img src={starWarsLogo} className="banner-text" alt={'Star wars'}/>
+      </Link>
       <Nav />
     </header>    
   );
