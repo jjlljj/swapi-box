44e77328db178eb5e3c87a25990b909b7e45dbcf
James Logue <logueja@gmail.com>
2018-02-03 20:51:34 -0700

    Begin mock of dataHelper functions for App tests

---
+++ src/dataHelper/__mocks__/dataHelper.js
@@ -0,0 +1,25 @@
+const getScroll = () => {
+  
+  console.log('fuckkkthisssx')
+}
+
+const getPeople = () => {
+  console.log('fuckkkthisssx')
+
+}
+
+const getPlanets = () => {
+  console.log('fuckkkthisssx')
+
+}
+
+const getVehicles = () => {
+  console.log('fuckkkthisss')
+}
+
+export {
+  getScroll,
+  getPeople,
+  getPlanets,
+  getVehicles
+}
