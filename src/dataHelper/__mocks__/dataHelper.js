60212d135a7b8a6d316f18a65a80af755a75cabc
James Logue <logueja@gmail.com>
2018-02-03 21:25:23 -0700

    Update mocks to return clean data to tests

--- src/dataHelper/__mocks__/dataHelper.js
+++ src/dataHelper/__mocks__/dataHelper.js
@@ -1,20 +1,75 @@
 const getScroll = () => {
-  
-  console.log('fuckkkthisssx')
+  fetch('https://swapi.co/api/films/')
+  return {
+    title: "A New Hope",
+    episode: 'Episode IV',
+    text: "It is a period of civil war..."
+  }
 }
 
 const getPeople = () => {
-  console.log('fuckkkthisssx')
-
+  fetch('https://swapi.co/api/people/')
+  return [
+    {
+      name: 'Luke skywalker',
+      cardType: "people",
+      Population: 10000,
+      Species: 'human',
+      Homeworld: "Tatooine",
+    },
+    {
+      name: 'C-3PO',
+      cardType: "people",
+      Population: 3244,
+      Species: 'droid',
+      Homeworld: "somewhere",
+    }
+  ]
 }
 
 const getPlanets = () => {
-  console.log('fuckkkthisssx')
+  fetch('https://swapi.co/api/planets/')
+  return [
+    {
+      Climate: "murky",
+      Population: "unknown",
+      Residents: "",
+      Terrain: "swamp, jungles",
+      cardType: "planets",
+      favorite: true,
+      name: "Dagobah"
+    },
+    {
+      Climate: "temperate, tropical", 
+      Population: "1000",
+      Residents: "",
+      Terrain: "jungle, rainforests",
+      cardType: "planets",
+      favorite: true,
+      name: "Yavin IV"
+    }
+  ]
 
 }
 
 const getVehicles = () => {
-  console.log('fuckkkthisss')
+  fetch('https://swapi.co/api/vehicles/')
+  return [
+    {
+      Model: "Digger Crawler",
+      Passengers: "30",
+      "Vehicle Class": "wheeled",
+      cardType: "vehicles",
+      name: "Sand Crawler"
+    },
+    {
+      Model: "T-16 skyhopper",  
+      Passengers: "1",
+      "Vehicle Class": "repulsorcraft",
+      cardType: "vehicles",
+      name: "T-16 skyhopper"
+    }
+  ]
 }
 
 export {
